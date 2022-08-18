acc064aab44c069b02db2178ec64717fa6eb6082
mingi-yoo <skys7297@naver.com>
2022-08-18 14:08:07 +0900

    I should add baseline dpu program

--- host.cpp
+++ host.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <limits>
 
 using namespace dpu;
 using namespace std;
@@ -51,11 +52,26 @@ void read_csr(string csr_path) {
         throw invalid_argument("Cannot open graph");
 }
 
-void populate_mram_base(DpuSetOps& dpu) {
-    // TO-DO
+void populate_mram(DpuSetOps& dpu) {
+    vector<uint32_t> row_buffer(NB_OF_VERTICES, numeric_limits<uint32_t>::max())
+    vector<uint32_t> col_buffer(NB_OF_EDGES, numeric_limits<uint32_t>::max())
+    vector<uint32_t> val_buffer(NB_OF_EDGES, numeric_limits<float>::max())
+
+    // copy data for passing to MRAM
+    for (int i = 0; i < row_ptr.size(); i++) {
+        row_buffer[i] = row_ptr[i];
+
+    for (int i = 0; i < col_idx.size(); i++) {
+        col_buffer[i] = col_idx[i];
+        val_buffer[i] = value[i];
+    }
+
+    dpu.copy("row_ptr", row_buffer, static_cast<unsigned>(NB_OF_VERTICES));
+    dpu.copy("col_idx", col_buffer, static_cast<unsigned>(NB_OF_EDGES));
+    dpu.copy("value", val_buffer, static_cast<unsigned>(NB_OF_EDGES));
 }
 
-void populate_mram_ours(DpuSetOps& dpu, uint32_t id) {
+void populate_mram(DpuSetOps& dpu, uint32_t id) {
     // TO-DO
 }
 
@@ -67,6 +83,10 @@ int main(int argc, char** argv) {
         auto system = DpuSet::allocate(NB_OF_DPUS);
         auto dpu_baseline = system.dpus()[0];
         dpu_baseline->load(DPU_BASELINE)
+        populate_mram(*dpu_baseline);
+        dpu->exec();
+
+        // TO-DO : ours
 
     } catch (const DpuError & e) {
         cerr << e.what() << endl;
