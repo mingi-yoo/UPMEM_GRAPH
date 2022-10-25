4b360183a1df843b115aa9c759bc8575198f2ba4
mingi-yoo <skys7297@naver.com>
2022-10-25 11:35:10 +0900

    debugging senario 2

--- senario_2_cpp/host/host.cpp
+++ senario_2_cpp/host/host.cpp
@@ -31,8 +31,7 @@ void populate_mram(DpuSetOps& dpu, Graph& graph, uint32_t id) {
     dpu.copy(DPU_MRAM_HEAP_POINTER_NAME, 0, graph.dpu_param[id], ROUND_UP_TO_MULTIPLE_OF_8(sizeof(DPUGraph)));
     dpu.copy(DPU_MRAM_HEAP_POINTER_NAME, graph.dpu_param[id][0].row_ptr_start, graph.row_ptr[id]);
     dpu.copy(DPU_MRAM_HEAP_POINTER_NAME, graph.dpu_param[id][0].col_idx_start, graph.col_idx[id]);
-    dpu.copy(DPU_MRAM_HEAP_POINTER_NAME, graph.dpu_param[id][0].value_start, graph.value);
-    dpu.copy(DPU_MRAM_HEAP_POINTER_NAME, graph.dpu_param[id][0].out_deg_start, graph.out_deg);
+    dpu.copy(DPU_MRAM_HEAP_POINTER_NAME, graph.dpu_param[id][0].fc_start, graph.fc);
 }
 
 void populate_mram_parallel(DpuSetOps& dpu, Graph& graph) {
@@ -94,72 +93,13 @@ int main(int argc, char** argv) {
 
         vector<vector<float>> result(NR_DPUS);
         for (uint32_t i = 0; i < NR_DPUS; i++)
-            result[i].resize(static_cast<unsigned>(graph.value.size()));
+            result[i].resize(static_cast<unsigned>(graph.fc.size()));
 
-        dpu_baseline->copy(result, static_cast<unsigned>(graph.value.size() * sizeof(float)), DPU_MRAM_HEAP_POINTER_NAME, graph.dpu_param[0][0].output_start);
+        dpu_baseline->copy(result, static_cast<unsigned>(graph.fc.size() * sizeof(float)), DPU_MRAM_HEAP_POINTER_NAME, graph.dpu_param[0][0].output_start);
         cout<<"OUTPUT RECEIVED"<<endl;
         for (uint32_t i = 0; i < 10; i++)
             cout<<"DPU RESULT: "<<result[0][i]<<endl;
 
-        // Ours
-        Graph subgraph = divide_graph(graph, NR_DPUS, num_t);
-
-        system.load(DPU_OURS);
-
-        cout<<"OURS PROGRAM ALLOCATED"<<endl;
-
-        begin = chrono::steady_clock::now();
-        for (uint32_t i = 0; i < NR_DPUS; i++) {
-            auto dpu = system.dpus()[i];
-            populate_mram(*dpu, subgraph, i);
-        }
-        end = chrono::steady_clock::now();
-        cout<<"DATA TRANSFER TIME: "<<chrono::duration_cast<chrono::nanoseconds>(end - begin).count() / 1.0e9 <<" secs"<<endl;
-        begin = chrono::steady_clock::now();
-        system.exec();
-        end = chrono::steady_clock::now();
-        cout<<"HOST ELAPSED TIME: "<<chrono::duration_cast<chrono::nanoseconds>(end - begin).count() / 1.0e9 <<" secs."<<endl;
-        for (uint32_t i = 0; i < NR_DPUS; i++) {
-            auto dpu = system.dpus()[i];
-            dpu->log(cout);
-        }
-        cout<<"OUTPUT RECEIVED"<<endl;
-        for (uint32_t i = 0; i < NR_DPUS; i++) {
-            auto dpu = system.dpus()[i];
-            dpu->copy(result, static_cast<unsigned>(subgraph.value.size() * sizeof(float)), DPU_MRAM_HEAP_POINTER_NAME, subgraph.dpu_param[i][0].output_start);
-            cout<<"DPU "<<i<<endl;
-            for (uint32_t j = 0; j < 5; j++) {
-                cout<<result[0][j]<<endl;
-            }
-            cout<<endl;
-        }
-
-        // OURS (parallel)
-
-        cout<<"OURS PARALLEL START"<<endl;
-        begin = chrono::steady_clock::now();
-        populate_mram_parallel(system, subgraph);
-        end = chrono::steady_clock::now();
-        cout<<"DATA TRANSFER TIME: "<<chrono::duration_cast<chrono::nanoseconds>(end - begin).count() / 1.0e9 <<" secs"<<endl;
-
-        begin = chrono::steady_clock::now();
-        system.exec();
-        end = chrono::steady_clock::now();
-        cout<<"HOST ELAPSED TIME: "<<chrono::duration_cast<chrono::nanoseconds>(end - begin).count() / 1.0e9 <<" secs."<<endl;
-        for (uint32_t i = 0; i < NR_DPUS; i++) {
-            auto dpu = system.dpus()[i];
-            dpu->log(cout);
-        }
-        system.copy(result, static_cast<unsigned>(subgraph.value.size() * sizeof(float)), DPU_MRAM_HEAP_POINTER_NAME, subgraph.dpu_param[0][0].output_start);
-
-        cout<<"OUTPUT RECEIVED"<<endl;
-        for (uint32_t i = 0; i < NR_DPUS; i++) {
-            cout<<"DPU "<<i<<endl;
-            for (uint32_t j = 0; j < 5; j++) {
-                cout<<result[i][j]<<endl;
-            }
-            cout<<endl;
-        }
     } catch (const DpuError & e) {
         cerr << e.what() << endl;
     }
